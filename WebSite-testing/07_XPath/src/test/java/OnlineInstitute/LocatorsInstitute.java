5a1ab17dea5dac843ba59c63e05ac6b37b82d05d
NickolayGorlanov <104831592+NickolayGorlanov@users.noreply.github.com>
2024-02-26 09:52:59 +0300

    modified

--- WebSite-testing/07_XPath/src/test/java/OnlineInstitute/LocatorsInstitute.java
+++ WebSite-testing/07_XPath/src/test/java/OnlineInstitute/LocatorsInstitute.java
@@ -3,8 +3,8 @@ package OnlineInstitute;
 import org.openqa.selenium.By;
 
 public  class LocatorsInstitute {
-    static By headerTitle = By.xpath("(//span[@class='baseCard__title'])[5]");
-    static By lastPeriodElement = By.xpath("(//div[@class='baseCondition'])[last()]");
+    static By headerTitle = By.xpath("(//span)[5]");
+    static By lastPeriodElement = By.xpath("(//div[@class='baseCondition']//p)[last()]");
     static By parentLink = By.xpath("//a[@href='#']/parent::div");
     static By fiveElement = By.xpath("(//div[@class='baseCondition'])[5]");
     static By headerAllCourses = By.xpath("(//div[@class='pageCreate__title']/ancestor::div)");
